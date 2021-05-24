f522c63507c91b5cd77b523c3bbe0bd67978eb23
RADIOM <hoggins@radiom.fr>
2021-05-24 10:05:48 +0200

    Sauvegarde

--- testCO2.ino
+++ testCO2.ino
@@ -14,8 +14,19 @@
   Open Serial Monitor at 115200 baud.
 */
 
+#include <math.h>
 #include <Wire.h>
 #include "co2rgb.h"
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+#include "time.h"
+#include <HTTPClient.h>
+#include "esp_wpa2.h"
+#include "EEPROM.h"
+
+char* ssid = "";
+char* pass = "";
 
 #include "SparkFun_SCD30_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD30
 SCD30 airSensor;
@@ -27,7 +38,96 @@ SCD30 airSensor;
 
 CO2RGB led(MINLVL, MEDLVL, HIGHLVL);
 
+WiFiMulti WiFiMulti;
+
 uint16_t co2;
+uint16_t temp;
+uint16_t humidity;
+
+uint16_t avg;
+uint16_t co2_arr[30] = {0};
+uint16_t temp_co2_arr[30] = {0};
+int taille = sizeof(co2_arr)/sizeof(uint16_t);
+
+/**
+ * This is lets-encrypt-x3-cross-signed.pem
+ */
+const char* rootCACertificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \
+"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \
+"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
+"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \
+"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \
+"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \
+"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \
+"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \
+"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \
+"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \
+"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \
+"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \
+"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \
+"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \
+"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \
+"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \
+"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \
+"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \
+"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \
+"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \
+"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \
+"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \
+"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \
+"-----END CERTIFICATE-----\n";
+
+uint16_t getAvg(uint16_t lastVal) {
+
+  uint16_t retour;
+
+  uint16_t prev_val;
+  uint16_t sum = 0;
+  
+  // on copie le tableau actuel dans un tableau temporaire
+  memcpy(temp_co2_arr, co2_arr, sizeof(co2_arr));
+  // case par case, on copie les valeurs en décalant
+  for(int i = 0; i < taille; i++) {
+    if(i == taille - 1) {
+      prev_val = lastVal;
+    }else{
+      prev_val = temp_co2_arr[i + 1];
+    }
+    
+    co2_arr[i] = prev_val;
+  }
+  // on calcule la moyenne finale
+  for(int i = 0; i < taille; i++) {
+    sum += co2_arr[i];
+  }
+
+  return ceil(sum/taille);
+}
+
+// Not sure if WiFiClientSecure checks the validity date of the certificate. 
+// Setting clock just to be sure...
+void setClock() {
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
 
 void setup()
 {
@@ -42,6 +142,21 @@ void setup()
       ;
   }
 
+  WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
+  WiFi.mode(WIFI_STA); //init wifi mode
+  WiFi.setHostname("youpi");
+  WiFiMulti.addAP(ssid, pass);
+  
+
+  // wait for WiFi connection
+  Serial.print("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  setClock();
+
   led.System();
   led.Sequence();
 
@@ -53,20 +168,24 @@ void loop()
   if (airSensor.dataAvailable())
   {
     co2 = airSensor.getCO2();
+    temp = airSensor.getTemperature();
+    humidity = airSensor.getHumidity();
+
+    avg = getAvg(co2);
     Serial.print("co2(ppm):");
     Serial.print(co2);
-    led.SetColor(co2);
+    Serial.print(" co2_avg(ppm):");
+    Serial.print(avg);
+
+    led.SetColor((avg < co2) ? avg : co2);
 
     Serial.print(" temp(C):");
-    Serial.print(airSensor.getTemperature(), 1);
+    Serial.print(temp, 1);
 
     Serial.print(" humidity(%):");
-    Serial.print(airSensor.getHumidity(), 1);
+    Serial.print(humidity, 1);
 
     Serial.println();
   }
-  else
-    Serial.println("Waiting for new data");
-
   delay(500);
 }
